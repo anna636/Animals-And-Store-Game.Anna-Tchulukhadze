4a920a99585a368eb8e5dadd2bdac86cb4ee4954
anna636 <anna.tchulukhadze@gmail.com>
2020-09-29 17:28:49 +0200

    Created field variables and constructor for Player class

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,25 @@
 package com.company;
+import java.util.*;
 
 public class Player {
+
+    String name;
+    String lastName;
+    int age;
+    int money=1100;
+
+
+    //Two array lists for animals and food that player stores
+    ArrayList<Animal> animals=new ArrayList<>();
+    ArrayList<Food> foodForAnimals=new ArrayList<>();
+
+
+    //Constructor to set field variables
+
+    public Player(String name, String lastName, int age)
+    {
+        this.name=name;
+        this.lastName=lastName;
+        this.age=age;
+    }
 }
